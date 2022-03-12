a4cc5b56403c4e8544633f9f38adfc35349bef13
Cody Martin <codytylermartin@gmail.com>
2022-03-12 16:34:07 -0700

    Add yapf settings and force format
    This leverages the fact that yapf looks for a file named
    .style.yapf in the same directory as the source. The settings
    are mostly pep8, except for a slightly wider column limit.
    Apply these settings with
    
        yapf -i -r ./

--- test_cmake_saver.py
+++ test_cmake_saver.py
@@ -1,9 +1,11 @@
 import cmake_saver
 import unittest
 
+
 class TestUserInterface(unittest.TestCase):
     def test_main(self):
         self.assertRaises(SystemExit, cmake_saver.main)
+
 
 if __name__ == '__main__':
     unittest.main()
