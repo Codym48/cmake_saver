ba9c960bc845a612472bc30da2e11e38c03df2cb
Cody Martin <codytylermartin@gmail.com>
2022-07-08 21:11:30 -0700

    Add failing test case

--- test_cmake_saver.py
+++ test_cmake_saver.py
@@ -7,6 +7,9 @@ class TestUserInterface(unittest.TestCase):
     def test_main(self):
         self.assertRaises(SystemExit, cmake_saver.main)
 
+    def test_this_should_fail(self):
+        self.assertEquals(1, 0)
+
 
 if __name__ == '__main__':
     unittest.main()
